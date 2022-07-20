fe9d774edc0641ba4b5c426c7b8687927125db5f
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2022-07-20 23:39:35 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home">
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}:</h3>
+    <div>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input type="text" v-model="counterData.title">
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { reactive, computed, watch, onMounted } from 'vue'
+
+  const appTitle = 'My Amazing Counter App'
+
+  onMounted(() => {
+    console.log('Do stuff related to App Title');
+  })
+
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter'
+  })
+
+  watch(() => counterData.count, (newCount, oldCount) => {
+    if(newCount === 20){
+      alert('Way to go! You made it 20!!')
+    }
+  })
+
+  const oddOrEven = computed(() => {
+    if(counterData.count % 2 === 0) return 'even'
+    return 'odd'
+  })
+
+  const increaseCounter = (amount, e) => {
+    counterData.count += amount
+  }
+
+  const decreaseCounter = amount => {
+    counterData.count -= amount
+  }
+
+  onMounted(() => {
+    console.log('Do stuff realted to Counter');
+  })
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+
+.edit{
+  margin-top: 60px;
+}
+</style>
